6b75a3f97ad4c7674562f6063491ef97a36983b4
itswl <imwl@live.com>
2018-12-18 10:39:25 +0800

    离线脚本批量创建,(软)删除用户

--- fake.py
+++ fake.py
@@ -1,14 +1,16 @@
 
-
+#  离线脚本
 
 from app import create_app
 from app.models.base import db
 from app.models.user import User
 
 app = create_app()
+
+
 with app.app_context():
     with db.auto_commit():
-        # 离线脚本，创建一个超级管理员
+        # 创建一个超级管理员
         user = User()
         user.nickname = 'Super'
         user.password = '123456'
@@ -19,7 +21,7 @@ with app.app_context():
 with app.app_context():
     for v in range(1, 100):
         with db.auto_commit():
-        # 离线脚本，批量创建普通用户
+        # 批量创建普通用户
             user = User()
             user.nickname = 'user'+str(v)
             user.password = '123456'
@@ -27,4 +29,20 @@ with app.app_context():
             user.auth = 1
             db.session.add(user)
 
-# 直接运行就能创建
+
+with app.app_context():
+    for v in range(2, 101):  # 得先查看id
+        with db.auto_commit():
+        # 批量软删除用户
+            id = int(v)
+            user = User.query.filter_by(id=id).first_or_404()
+            user.delete() # base中有定义delete,将status改为0
+            db.session.add(user)
+
+with app.app_context():
+    for v in range(2, 101):
+        with db.auto_commit():
+        # 批量删除用户
+            id = int(v)
+            user = User.query.get(id)  # 因为filiter_by有改写，不能查到status=0
+            db.session.delete(user)
